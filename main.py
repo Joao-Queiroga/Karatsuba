2a0bb704bc16d1463ea03c9b96ddb59f6ca1bbe5
Joao-Queiroga <queiroga.joaofcsol@gmail.com>
2025-08-27 12:36:08 -0300

    adiciona algoritimo em python

---
+++ main.py
@@ -0,0 +1,28 @@
+def dividir_numero(n: int, m: int):
+    s = str(n)
+    if len(s) <= m:
+        return 0, m
+    return int(s[:-m]), int(s[-m:])
+
+
+def karatsuba(x: int, y: int):
+    if abs(x) < 10 or abs(y) < 10:
+        return x * y
+
+    n = max(len(str(x)), len(str(y)))
+    m = n // 2
+
+    a, b = dividir_numero(x, m)
+    c, d = dividir_numero(y, m)
+
+    p1 = karatsuba(a, c)
+    p2 = karatsuba(b, d)
+    p3 = karatsuba(a + b, c + d)
+
+    m = max(len(str(x)), len(str(y))) // 2
+
+    return p1 * 10 ** (2 * m) + (p3 - p1 - p2) * 10**m + p2
+
+
+print(karatsuba(1234, 5678))
+print("checagem = ", 1234 * 5678)
